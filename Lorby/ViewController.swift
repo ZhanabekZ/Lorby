27011f456fa00dc3200369334a46d272053c417b
ZhanabekZ <zhanabekzinekenov@gmail.com>
2024-04-13 13:21:56 +0500

    Initial Commit

---
+++ Lorby/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lorby
+//
+//  Created by ZhZinekenov on 13.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
